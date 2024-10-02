8d907eedddb0fb31be0b902eb3a6bc0f037eda13
luiskmd <49592247+luis-kmd@users.noreply.github.com>
2024-10-02 19:14:11 -0300

    Update Especialidade.java

--- 2770-spring-boot-aula_2/src/main/java/med/voll/api/domain/medico/Especialidade.java
+++ 2770-spring-boot-aula_2/src/main/java/med/voll/api/domain/medico/Especialidade.java
@@ -2,9 +2,9 @@ package med.voll.api.domain.medico;
 
 public enum Especialidade {
 
-    ORTOPEDIA,
-    CARDIOLOGIA,
-    GINECOLOGIA,
-    DERMATOLOGIA;
+    ORTOPEDISTA,
+    CARDIOLOGISTA,
+    GINECOLOGISTA,
+    DERMATOLOGISTA;
 
 }
